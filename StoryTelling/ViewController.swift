7fbb7f916ac39fb42d4ae9b131326040e5bec5d2
Baims <baimsapps@gmail.com>
2015-07-16 03:20:28 +0300

    Initial Commit

---
+++ StoryTelling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryTelling
+//
+//  Created by Bader Alrshaid on 7/16/15.
+//  Copyright (c) 2015 Bader Alrshaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
